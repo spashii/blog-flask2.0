faa766e50b2d9996a8bbed7dccdbe4adf1a1eb88
spashii <sameer.pashikanti@gmail.com>
2020-04-17 22:36:54 +0530

    templates and routes

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,19 @@
+from flask import render_template, url_for
 from app import app
 
 
 @app.route('/')
 @app.route('/home')
 def index():
-    return "Hello, World!"
-
+    user = {'username': 'Sameer'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful say in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'I have a cool handbag.'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)

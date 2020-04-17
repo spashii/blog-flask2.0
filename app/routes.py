509c0f43b600c60be0d7c38e2121a16c009b3610
spashii <sameer.pashikanti@gmail.com>
2020-04-18 00:09:26 +0530

    validators for login

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, url_for
+from flask import render_template, url_for, flash, redirect
 from app import app
 from app.forms import LoginForm
 
@@ -19,7 +19,10 @@ def index():
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        flash(f'Login requested for user {form.username.data}, remember me{form.remember_me.data}')
+        return redirect(url_for('index'))
     return render_template('login.html', title='Login', form=form)

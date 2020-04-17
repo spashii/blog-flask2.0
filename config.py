1860cd30e9664683b5ba07c2f597250d5401cd5d
spashii <sameer.pashikanti@gmail.com>
2020-04-18 02:22:43 +0530

    added models, database, flask-migrate

--- config.py
+++ config.py
@@ -1,4 +1,8 @@
 import os
+base_dir = os.path.abspath(os.path.dirname(__file__))
+
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or '12345678'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI') or 'sqlite:///' + os.path.join(base_dir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False

5afcefa342212791facc23a441b39eeb3c408223
spashii <sameer.pashikanti@gmail.com>
2020-04-19 15:27:18 +0530

    added: unit testing, models bug fix

--- app/models.py
+++ app/models.py
@@ -4,6 +4,12 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from hashlib import md5
 
+followers = db.Table(
+    'followers',
+    db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('followed_id', db.Integer, db.ForeignKey('user.id')))
+
+
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
@@ -12,6 +18,13 @@ class User(db.Model, UserMixin):
     posts = db.relationship('Post', backref='author', lazy='dynamic')
     bio = db.Column(db.String(128))
     last_seen = db.Column(db.DateTime, default=datetime.utcnow)
+    followed = db.relationship(
+        'User',
+        secondary=followers,
+        primaryjoin=(followers.c.follower_id == id),
+        secondaryjoin=(followers.c.followed_id == id),
+        backref=db.backref('followers', lazy='dynamic'),
+        lazy='dynamic')
 
     def __repr__(self):
         return f'<User {self.username}>'
@@ -26,24 +39,9 @@ class User(db.Model, UserMixin):
         digest = md5(self.email.encode('utf-8')).hexdigest()
         return f'https://gravatar.com/avatar/{digest}?d=identicon&s={size}'
 
-    followers = db.Table(
-        'followers',
-        db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
-        db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
-    )
-    followed = db.relationship(
-        'User',
-        secondary=followers,
-        primaryjoin=(followers.c.follower_id == id),
-        secondaryjoin=(followers.c.followed_id == id),
-        backref=db.backref('followers', lazy='dynamic'),
-        lazy='dynamic'
-    )
-
     def is_following(self, user):
-        return self.followed.fliter(
-            followers.c.followed_id == user.id
-        ).count() > 0
+        return self.followed.filter(
+            followers.c.followed_id == user.id).count() > 0
 
     def follow(self, user):
         if not self.is_following(user):
@@ -56,9 +54,8 @@ class User(db.Model, UserMixin):
     def followed_posts(self):
         followed = Post.query.join(
             followers, (followers.c.followed_id == Post.user_id)).filter(
-                followers.c.follower_id == self.id).order_by(
-                    Post.timestamp.desc())
-        own = Post.query.filter_by(user_id == self.id)
+                followers.c.follower_id == self.id)
+        own = Post.query.filter_by(user_id=self.id)
         return followed.union(own).order_by(Post.timestamp.desc())
             
                     

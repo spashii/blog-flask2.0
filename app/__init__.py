6a939fc6d069952a98a370cf84259f24fbed2bca
spashii <sameer.pashikanti@gmail.com>
2020-04-20 00:01:38 +0530

    added: flask-mail

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_mail import Mail
 import os, logging
 from logging.handlers import SMTPHandler, RotatingFileHandler
 
@@ -12,6 +13,7 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
+mail = Mail(app)
 
 from app import routes, models, errors
 
--- config.py
+++ config.py
@@ -12,4 +12,4 @@ class Config:
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['info.sameerpashikanti@gmail.com']
-    POSTS_PER_PAGE = 5
+    POSTS_PER_PAGE = 12
